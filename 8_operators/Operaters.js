acb07e11a8cef8de43a0f7fe0f2af907867b852b
Lo Tsz Kin <ltk.work123@gmail.com>
2025-04-23 12:46:11 +0800

    operaters

---
+++ 8_operators/Operaters.js
@@ -0,0 +1,30 @@
+console.log(5%1);//5 mod by 1
+
+
+console.log(1%0);
+
+console.log(1/0);
+
+
+let x = 23;
+let ans;
+ans = x%=2;
+
+console.log(ans);
+
+
+console.log("-------------------------------------")
+
+
+//Precedence of operators運算符的優先級
+
+ans = 1+2*3; //first * then +
+console.log(ans);
+
+//If want to add 1 and 2 first then Need()
+
+ans = (1+2)*3;
+console.log(ans)
+
+ans ++;
+console.log(ans)
---
+++ 8_operators/assaociativity.js
@@ -0,0 +1,10 @@
+let a = 20-10-5;
+console.log(a)
+
+let y =0;
+let x = 0;
+x=y=x+1; //Calculater from right to left == x+1 -> y -> x == x = y = 1 == x=1 
+console .log(x,y); //Print X and Y
+
+
+
