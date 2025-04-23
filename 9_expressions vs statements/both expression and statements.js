aba39c6c3b276012823d0ed6e699941933bbb365
Lo Tsz Kin <ltk.work123@gmail.com>
2025-04-23 13:07:35 +0800

    9

---
+++ 9_expressions vs statements/both expression and statements.js
@@ -0,0 +1,6 @@
+let i = 0;
+console.log((i=100));
+console.log(i++);//Will display 100 , because is only run after this line //both expression and statements
+console.log(i);//both expression and statements
+
+
