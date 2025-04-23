c9b03729b5758cba29b0d308fd652c3f1ba9f9b5
Lo Tsz Kin <ltk.work123@gmail.com>
2025-04-23 12:14:13 +0800

    Types

---
+++ 7_basic types/Difft-type.js
@@ -0,0 +1,36 @@
+console.log("Hello world");
+ 
+
+
+//can not do (i'm xxx' ) , will error , should add a \
+//console.log('i'm james');
+console.log('i\'m james');
+
+console.log(123+456);
+
+
+// let a = "i'm";
+// let b = "james";
+// consloe.log("i'm"+"james")//will error
+
+
+
+// consloe.log('123'+'456'); // will error consider as undefind
+
+// consloe.log("123"+456); // will error
+
+
+
+const str = "hello world";
+
+console.log(str[0]) // will display the first string of the str 
+
+console.log(str.length) //Will display the length of the string , in this case will be 11
+
+console.log("123".length)//Will display the length of the front , in this case will be count the length of 123 and the answer will be 3
+
+
+const a = 1;
+
+console.log(typeof str); //Will display the type of const str , i in this case will be string
+console.log(typeof a);
