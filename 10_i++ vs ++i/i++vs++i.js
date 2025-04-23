94a6480c81d08a646d687dd063a9f280e554c022
Lo Tsz Kin <ltk.work123@gmail.com>
2025-04-23 14:19:40 +0800

    10 and 9

---
+++ 10_i++ vs ++i/i++vs++i.js
@@ -0,0 +1,9 @@
+let i = 0;
+console.log(i++)
+console.log(i);
+console.log("-----------------")
+
+//
+i=0;//reset i
+console.log(++i)
+console.log(i);
---
+++ 9_expressions vs statements/Expressions as statements.js
@@ -0,0 +1,4 @@
+3; //Statement
+1+3;
+"hello" + "world";// Will not show anything
+
