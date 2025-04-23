cd84eedabfb76f858c0f154caba8ae022af2aa13
Lo Tsz Kin <ltk.work123@gmail.com>
2025-04-23 11:07:19 +0800

    console log useage

---
+++ 1_console.log/Assignment/assignment.js
@@ -0,0 +1,37 @@
+//Making a variable to change dynamic
+
+let myHP = 100;
+console.log(myHP);
+
+myHP =200; //Can not use Let , the coder will say error , because it is only allow to crete a new var
+console.log(myHP)
+
+myHP = 1+200+3; //Can also do some complax stuff
+console.log(myHP)
+
+
+myHP = myHP +3; //Dynamic add number
+console.log(myHP)
+
+
+myHP +=3; //A compex/lazy way to do same add number
+console.log(myHP)
+
+
+myHP -=3; 
+console.log(myHP)
+
+
+myHP *=3; 
+console.log(myHP)
+
+
+myHP /=3; 
+console.log(myHP)
+
+
+myHP++; // Add 1 from the var
+console.log(myHP)
+
+myHP--;// -1 from the var
+console.log(myHP)
---
+++ 1_console.log/Variable/variable.js
@@ -0,0 +1,18 @@
+let a = 1; //Add a variable 1(int) as a 
+
+console.log(a);  //print it out
+
+
+let characterHP = 100;
+
+
+let NoInitialValue; //A undefind variable
+console.log(NoInitialValue);
+
+
+let sum = a + characterHP; //Create a new variable to save added of a and characterHP
+console.log(sum)
+
+
+let min = characterHP-a;
+console.log(min)
